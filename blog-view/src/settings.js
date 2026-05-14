3d41de7a7abd4acac2067227a04c5aa84eb7ad3b
liuna <sodium88@qq.com>
2026-05-14 16:49:20 +0800

    fix: update settings.js background images

--- blog-view/src/settings.js
+++ blog-view/src/settings.js
@@ -3,8 +3,8 @@ module.exports = {
 	 * @type {string}
 	 * @description 首页三张背景图
 	 */
-	bg1:'https://github.com/lanxing68/NBlog-ln/blob/main/%E5%B1%8F%E5%B9%95%E6%88%AA%E5%9B%BE%202026-05-14%20160334.png',
-
+	bg1:'https://cdn.naccl.top/blog/img/bg1.jpg',
+    bg2:'https://cdn.naccl.top/blog/img/bg2.jpg',
 	bg3: 'https://cdn.naccl.top/blog/img/bg3.jpg',
 
 	/**
